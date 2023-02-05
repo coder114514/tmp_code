b19edd61f533ecd42f50c0d0ba07809524ae7fe0
coder114514 <98397499+coder114514@users.noreply.github.com>
2023-02-05 23:16:57 +0800

    update lis_map (longest increasing sequence)

--- lis_map.cpp
+++ lis_map.cpp
@@ -1,23 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef pair<int,int> pii;
+//https://wwwwodddd.com/public/lis.html
+
+typedef pair<int, int> pii;
 
 int t, n;
 map<int, pii> f;
 int a[20020];
 
-void ins(int x, int y,int cnt) {
-	auto it = prev(f.lower_bound(x));
-	if (it->second.first >= y)
-	{
-		return;
-	}
-	it++;
-    while (it != f.end() && it->second.first < y) {
+//f[v]: 表示以v结尾的最长上升子序列 first是长度 second是数量
+
+//维护f满足单调性：v1<v2, f[v1].first<=f[v2].first
+void ins(int x, int y, int cnt) {
+    auto it = prev(f.lower_bound(x));
+    if (it->second.first >= y) {
+        return;
+    }
+    it++;
+    while (it != f.end() && it->second.first < y) {  //结尾数字>=x但是长度<y的子序列都比当前的差，所以全部删掉
         f.erase(it++);
     }
-    f[x] = make_pair(y,cnt);
+    f[x] = make_pair(y, cnt);
 }
 
 int main() {
@@ -26,19 +30,18 @@ int main() {
         scanf("%d", &a[i]);
     }
     f.clear();
-    f[-1e9] = make_pair(0,1);
+    f[-1e9] = make_pair(0, 1);
     for (int i = 0; i < n; i++) {
-        auto it=f.lower_bound(a[i]);
-        int len=prev(it)->second.first,cnt=0;
-        do
-        {
+        auto it = f.lower_bound(a[i]);
+        int len = prev(it)->second.first, cnt = 0;
+        do {
             --it;
-            if(it->second.first!=len)break;
-            cnt+=it->second.second;
-        }while(it!=f.begin());
-        ins(a[i],len + 1,cnt);
+            if (it->second.first != len)
+                break;
+            cnt += it->second.second;
+        } while (it != f.begin());
+        ins(a[i], len + 1, cnt);
     }
-    printf("%d %d\n", f.rbegin()->second.first,f.rbegin()->second.second);
+    printf("%d %d\n", f.rbegin()->second.first, f.rbegin()->second.second);
     return 0;
 }
-
