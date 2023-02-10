edf23d0ec96e7a1f867e740624ea3fc3521703e4
coder114514 <98397499+coder114514@users.noreply.github.com>
2023-02-10 22:15:21 +0800

    Update -10进制.cpp

--- -10进制.cpp
+++ -10进制.cpp
@@ -1,33 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int bits[1010],nB;
+int bits[1010], nB;
 
 void change(int n) {
-    nB=0;
-    while(n) {
-        int m=(n>0?n/-10:(-n+9)/10);
-        bits[++nB]=n-m*-10;
-        n=m;
+    nB = 0;
+    while (n) {
+        int m = (n > 0 ? n / -10 : (-n + 9) / 10);
+        bits[++nB] = n - m * -10;
+        n = m;
     }
-    if(!nB)bits[++nB]=0;
+    if (!nB) bits[++nB] = 0;
 }
 
 int main() {
-    for(int i=-100; i<=100; i++) {
+    for (int i = -100; i <= 100; i++) {
         change(i);
-        printf("%6d   ",i);
+        printf("%6d   ", i);
         change(i);
-        for(int j=nB; j>=1; j--) {
-            printf("%d",bits[j]);
+        for (int j = nB; j >= 1; j--) {
+            printf("%d", bits[j]);
         }
         printf("\n");
     }
     int n;
-    cin>>n;
+    cin >> n;
     change(n);
-    for(int j=nB; j>=1; j--) {
-        printf("%d",bits[j]);
+    for (int j = nB; j >= 1; j--) {
+        printf("%d", bits[j]);
     }
     printf("\n");
     return 0;
