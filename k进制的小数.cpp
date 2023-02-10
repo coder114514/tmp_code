c9d2d092fb94b9297b116672057836dca0dbd12e
coder114514 <98397499+coder114514@users.noreply.github.com>
2023-02-10 23:24:43 +0800

    Update k进制的小数.cpp

--- k进制的小数.cpp
+++ k进制的小数.cpp
@@ -8,47 +8,57 @@ map<ull, ull> occur;
 
 ull cnt(ull x) {
     ull res = 0;
-    while (x > 0)
-        ++res, x /= 10;
+    while (x > 0) {
+        ++res;
+        x /= 10;
+    }
     return res;
 }
 
 void putnum(ull x) {
-    if (x <= 0)
-        return;
+    if (x <= 0) return;
     putnum(x / k);
-    if (x % k >= 10)
-        cout << '(' << x % k << ')', len += cnt(x % k) + 2;
-    else
-        cout << x % k, len++;
+    if (x % k >= 10) {
+        cout << '(' << x % k << ')';
+        len += cnt(x % k) + 2;
+    }
+    else {
+        cout << x % k;
+        ++len;
+    }
 }
 
 int main() {
     cout << "输入x,y,k,输出x/y在k进制下的小数形式(打波浪线的是循环节,若小数某位大于9,就会打上括号)" << endl;
     cin >> x >> y >> k;
-    if (x < y)
-        cout << '0', len = 1;
-    else
+    if (x < y) {
+        cout << '0';
+        len = 1;
+    }
+    else {
         putnum(x / y);
+    }
     x = (x % y) * k;
     if (x == 0) {
         return 0;
     }
     cout << '.', ++len;
-    while (occur[x] == 0) {
+    while (!occur[x]) {
         occur[x] = len;
-        if (x / y >= 10)
-            cout << '(' << x / y % k << ')', len += cnt(x / y % k) + 2;
-        else
-            cout << x / y, ++len;
+        if (x / y >= 10) {
+            cout << '(' << x / y % k << ')';
+            len += cnt(x / y % k) + 2;
+        }
+        else {
+            cout << x / y;
+            ++len;
+        }
         x = (x % y) * k;
     }
     cout << endl;
-    for (ull j = 1; j <= len; j++)
-        if (j <= occur[x])
-            cout << ' ';
-        else
-            cout << '^';
+    for (ull j = 1; j <= len; j++) {
+        cout << (j <= occur[x] ? ' ' : '^');
+    }
     cout << endl;
     return 0;
 }
